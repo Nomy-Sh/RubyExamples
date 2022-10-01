2865c6f1df9115c8d8e50b45ad56c1d85e176b00
Noman Sheikh <noman.sheikh@copart.com>
2022-10-01 20:53:51 +0530

    Array with different data types

---
+++ arrays/file4.rb
@@ -0,0 +1,11 @@
+## This file contains example of Arrays with different data types in Ruby.
+
+## Now as you can see in list below, there are different data types(strings, numbers) in same list
+some_list = [1, 3, "hello", "oye"]
+
+puts some_list[0..3]	## Printing out stuff
+# =>
+# 1
+# 3
+# hello
+# oye
