c791e8a1d5f3b9be95c3db58310e1640af7eb637
Noman Sheikh <noman.sheikh@copart.com>
2022-10-01 20:53:22 +0530

    Array Slicing

---
+++ arrays/file3.rb
@@ -0,0 +1,14 @@
+## This file contains example of Array slicing in Ruby.
+
+## Let's consider an array with element "Bus", "Car", "Bike", "Bicycle", "Taxi", "Truck"
+vehicles = ["Bus", "Car", "Bike", "Bicycle", "Taxi", "Truck"]
+
+## To access 1st 3 element of array, i.e. "Bus", "Car", "Bike"
+puts vehicles[0..2]  		
+# => 
+# "Bus"  
+# "Car"
+# "Bike"
+
+print vehicles[0..2] 
+# => ["Bus", "Car", "Bike"]
